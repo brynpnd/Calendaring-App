5df58a061e088184bbde209da8afdfd9b87421b4
Bryan Pineda <brynpnd@gmail.com>
2015-04-03 17:42:17 -1000

    Fixed start time sorting bug

--- src/Calendar.java
+++ src/Calendar.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class Calendar {
 
+	public final String VERSION = "VERSION:2.0";
 	public final String TIMEZONE = "TZID=Pacific/Honolulu:";
 	
 	private Scanner scan;
@@ -36,8 +37,8 @@ public class Calendar {
 	
 	public void createICSFile() {
 		
-		String filename, version, classification, location, summary, priority,
-		DTSTART, DTEND, timeZone;
+		String filename, classification, location, summary, priority,
+		DTSTART, DTEND;
 		
 		classification = new String("");
 		priority = new String("");
@@ -45,7 +46,6 @@ public class Calendar {
 		System.out.print("Enter the file name to be saved (no spaces): ");
 
 		filename = scan.next();
-		version = "VERSION:2.0";
 		
 		boolean inputValid = false;
 		while(inputValid == false) {
@@ -96,25 +96,8 @@ public class Calendar {
 
 		DTSTART = "DTSTART;" + TIMEZONE + startDate + "T" + startTime;
 		DTEND = "DTEND;" + TIMEZONE + endDate + "T" + endTime;
-
-		try {
-			PrintWriter out = new PrintWriter(filename + ".ics");	
-			out.println("BEGIN:VCALENDAR");
-			out.println(version);
-			out.println("BEGIN:VEVENT");
-			out.println(classification);
-			out.println(priority);
-			out.println(location);
-			out.println(summary);
-			out.println(DTSTART);
-			out.println(DTEND);
-			out.println("END:VEVENT");
-			out.println("END:VCALENDAR");
-			out.close();
-		} catch(Exception e) {
-			System.out.println("PrintWriter error");
-			System.exit(1);
-		}
+		
+		printICSFile(filename, VERSION, classification, priority, location, summary, DTSTART, DTEND);
 		
 		System.out.println("Complete!");
 		
@@ -184,7 +167,6 @@ public class Calendar {
 				
 				try {
 					fileStrings.add(new String(data, "UTF-8"));
-					System.out.println(fileStrings.get(i));
 				} catch (UnsupportedEncodingException e) {
 					e.printStackTrace();
 				}
@@ -211,16 +193,219 @@ public class Calendar {
 
 			startDateStrings.add(currString.substring(start, start+8));
 			startTimeStrings.add(currString.substring(start+9,end));
+			
+			start = end;
+			
+			while(!Character.isDigit(currString.charAt(start))) 
+				start++;
+			
+			end = start;
+			
+			while(currString.charAt(end) != '\n')
+				end++;
+			
+			end--;
+			
+			endDateStrings.add(currString.substring(start,start+8));
+			endTimeStrings.add(currString.substring(start+9,end));
+			
 		}
 				
-		// Find dates and times between .ics files
+		/*** Find dates and times between .ics files ***/
+		List<Event> events = new ArrayList<Event>();
+		
+		for(int i = 0; i < startTimeStrings.size(); i++) {
+			events.add(new Event(startDateStrings.get(i),endDateStrings.get(i),startTimeStrings.get(i),endTimeStrings.get(i)));
+		}
+	
+		// Sort events by start date
+		for(int i = 0; i < events.size(); i++) {
+			
+			Event currEvent = events.get(i);
+			
+			for(int j = i+1; j < events.size(); j++) {
+				Event nextEvent = events.get(j);
+				if(currEvent.getStartDate() > nextEvent.getStartDate()) {
+					
+					events.set(i, nextEvent);
+					events.set(j, currEvent);
+					
+				}
+					
+			}	
+		}
+		
+		// Sort events by start time
+		for(int i = 0; i < events.size(); i++) {
+			
+			Event currEvent = events.get(i);
+			
+			int k = i;
+			for(int j = i+1; j < events.size(); j++) {
+				Event nextEvent = events.get(j);
+				if(currEvent.getStartDate() == nextEvent.getStartDate()) {
+					
+					if(currEvent.getStartTime() > nextEvent.getStartTime()) {
+						events.set(k, nextEvent);
+						events.set(j, currEvent);
+						k = j;
+					}
+					
+				}
+			}
+			// if a swap was made
+			if(k != i)
+				i--;
+		}
+
+		System.out.println("Events");
+		for(int i = 0; i < events.size(); i++) {
+			System.out.println(String.valueOf(events.get(i).getStartDate()));
+			System.out.println(String.valueOf(events.get(i).getStartTime()));
+			System.out.println(String.valueOf(events.get(i).getEndDate()));
+			System.out.println(String.valueOf(events.get(i).getEndTime()));
+		}
+
+		
+		// Find gaps between dates and times
+		List<Event> gaps = new ArrayList<Event>();
+		
+		for(int i = 0; i < events.size()-1; i++) {
+			
+			Event currEvent = events.get(i);
+			Event nextEvent = events.get(i+1);
+			
+			if(currEvent.getEndDate() < nextEvent.getStartDate()) {
+				gaps.add(new Event(currEvent.getEndDate(), nextEvent.getStartDate(), currEvent.getEndTime(), nextEvent.getStartTime()));
+			}
+			
+			else if(currEvent.getEndDate() == nextEvent.getStartDate()) {
+				
+				if(currEvent.getEndTime() < nextEvent.getStartTime()) {
+					
+					gaps.add(new Event(currEvent.getEndDate(), nextEvent.getStartDate(), currEvent.getEndTime(), nextEvent.getStartTime()));
+					
+				}
+			}
+			
+			else {
+				System.out.println("Error in sorting events!");
+				System.exit(1);
+			}
+			
+		}
+		
+		System.out.println("Gaps");
+		for(int i = 0; i < gaps.size(); i++) {
+			System.out.println(String.valueOf(gaps.get(i).getStartDate()));
+			System.out.println(String.valueOf(gaps.get(i).getStartTime()));
+			System.out.println(String.valueOf(gaps.get(i).getEndDate()));
+			System.out.println(String.valueOf(gaps.get(i).getEndTime()));
+		}
+		
 		
 		// Create new .ics files with date and times between .ics files
+		for(int i = 0; i < gaps.size(); i++) {
 			
-
+			Event currGap = gaps.get(i);
+			
+			String DTSTART = "DTSTART;" + TIMEZONE + currGap.getStartDateString() + "T" + currGap.getStartTimeString();
+			String DTEND = "DTEND;" + TIMEZONE + currGap.getEndDateString() + "T" + currGap.getEndTimeString();
+			
+			printICSFile("freetime_" + String.valueOf(i), VERSION, "CLASS:PUBLIC", "PRIORITY:1", "LOCATION:",
+					"SUMMARY:Free Time", DTSTART, DTEND);
+		}
+		
+		System.out.println("Complete!");
+		
+	}
+	
+	public void printICSFile(String filename, String version, String classification, String priority, String location, String summary, String DTSTART, String DTEND) {
+		
+		try {
+			PrintWriter out = new PrintWriter(filename + ".ics");	
+			out.println("BEGIN:VCALENDAR");
+			out.println(version);
+			out.println("BEGIN:VEVENT");
+			out.println(classification);
+			out.println(priority);
+			out.println(location);
+			out.println(summary);
+			out.println(DTSTART);
+			out.println(DTEND);
+			out.println("END:VEVENT");
+			out.println("END:VCALENDAR");
+			out.close();
+		} catch(Exception e) {
+			System.out.println("PrintWriter error");
+			System.exit(1);
+		}
 		
 	}
 		
+	public class Event {
+		
+		private int startTime;
+		private int endTime;
+		private int startDate;
+		private int endDate;
+		
+		public Event(int startD, int endD, int startT, int endT) {
+			
+			startDate = startD;
+			endDate = endD;
+			startTime = startT;
+			endTime = endT;
+			
+		}
+		
+		public Event(String startD, String endD, String startT, String endT) {
+			
+			startDate = Integer.parseInt(startD);
+			endDate = Integer.parseInt(endD);
+			startTime = Integer.parseInt(startT);
+			endTime = Integer.parseInt(endT);
+			
+		}
+		
+		public int getStartDate() {
+			return startDate;
+		}
+		
+		public int getEndDate() {
+			return endDate;
+		}
+		
+		public int getStartTime() {
+			return startTime;
+		}
+		
+		public int getEndTime() {
+			return endTime;
+		}
+		
+		public String getStartDateString() {
+			return String.valueOf(startDate);
+		}
+		
+		public String getEndDateString() {
+			return String.valueOf(endDate);
+		}
+		
+		public String getStartTimeString() {
+			if(startTime < 100000)
+				return "0" + String.valueOf(startTime);
+			else
+				return String.valueOf(startTime);
+		}
+		
+		public String getEndTimeString() {
+			if(endTime < 100000)
+				return "0" + String.valueOf(endTime);
+			else
+				return String.valueOf(endTime);
+		}
+	}
 
 	
 }
