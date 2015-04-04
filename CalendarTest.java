25f0adb73675571b4829c635e2f8563abbc8ff4e
cryosx <cyruswu.email@gmail.com>
2015-04-03 21:12:57 -1000

    Update CalendarTest.java

--- CalendarTest.java
+++ CalendarTest.java
@@ -1,8 +1,13 @@
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
-import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.*;
 
 
 /**
@@ -13,6 +18,9 @@ import static org.junit.Assert.assertEquals;
  * @since <pre>Apr 3, 2015</pre>
  */
 public class CalendarTest {
+
+
+    Calendar cal ;
 
     @Before
     public void before() throws Exception {
@@ -28,9 +36,7 @@ public class CalendarTest {
     @Test
     public void testMain() throws Exception {
 
-
-    //TODO: Test goes here...
-
+        //TODO: Test goes here...
 
     }
 
@@ -40,9 +46,7 @@ public class CalendarTest {
     @Test
     public void testCreateICSFile() throws Exception {
 
-
-    //TODO: Test goes here...
-
+        //TODO: Test goes here...
 
     }
 
@@ -52,9 +56,98 @@ public class CalendarTest {
     @Test
     public void testFindFreeTime() throws Exception {
 
-    //TODO: Test goes here...
+        //TODO: Test goes here...
+
+    }
+
+    /**
+     * Method: printICSFile(String filename, String version, String classification, String priority, String location, String summary, String DTSTART, String DTEND)
+     */
+    @Test
+    public void testPrintICSFile() throws Exception {
+
+        //TODO: Test goes here...
 
     }
 
 
-} 
+    Calendar.Event event;
+
+
+    /**
+     * Method: getStartDate()
+     */
+    @Test
+    public void testGetStartDate() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+
+
+    /**
+     * Method: getEndDate()
+     */
+    @Test
+    public void testGetEndDate() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: getStartTime()
+     */
+    @Test
+    public void testGetStartTime() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: getEndTime()
+     */
+    @Test
+    public void testGetEndTime() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: getStartDateString()
+     */
+    @Test
+    public void testGetStartDateString() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: getEndDateString()
+     */
+    @Test
+    public void testGetEndDateString() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: getStartTimeString()
+     */
+    @Test
+    public void testGetStartTimeString() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: getEndTimeString()
+     */
+    @Test
+    public void testGetEndTimeString() throws Exception {
+
+        //TODO: Test goes here...
+    }
+
+
+}
+
