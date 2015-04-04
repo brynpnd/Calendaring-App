90f8a1babdf1f86a7dda51ebc288740bd49f65ac
cryosx <cyruswu.email@gmail.com>
2015-04-03 17:36:31 -1000

    Create CalendarTest.java
    Basic Framework for Bryan's Calendar Class.

---
+++ CalendarTest.java
@@ -0,0 +1,60 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Calendar Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>Apr 3, 2015</pre>
+ */
+public class CalendarTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: main(String args[])
+     */
+    @Test
+    public void testMain() throws Exception {
+
+
+    //TODO: Test goes here...
+
+
+    }
+
+    /**
+     * Method: createICSFile()
+     */
+    @Test
+    public void testCreateICSFile() throws Exception {
+
+
+    //TODO: Test goes here...
+
+
+    }
+
+    /**
+     * Method: findFreeTime()
+     */
+    @Test
+    public void testFindFreeTime() throws Exception {
+
+    //TODO: Test goes here...
+
+    }
+
+
+} 
